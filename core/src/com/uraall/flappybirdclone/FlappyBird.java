b5aff801967b2728eecb771f2be87bbf2253ccdf
AlAndreev92 <brabusmax@mail.ru>
2020-03-17 00:29:40 +0300

    Flappy Bird Commit delete method render and dispose

--- core/src/com/uraall/flappybirdclone/FlappyBird.java
+++ core/src/com/uraall/flappybirdclone/FlappyBird.java
@@ -18,16 +18,11 @@ public class FlappyBird extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 }
