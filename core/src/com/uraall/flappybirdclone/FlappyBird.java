6023c8f8bbe631fb684cf564c6cecee40ff4163b
AlAndreev92 <brabusmax@mail.ru>
2020-03-17 16:59:52 +0300

    Flappy Bird Commit Bird falling (Прорисовка трубы)

--- core/src/com/uraall/flappybirdclone/FlappyBird.java
+++ core/src/com/uraall/flappybirdclone/FlappyBird.java
@@ -14,6 +14,9 @@ public class FlappyBird extends ApplicationAdapter {
 	float flyHeight;
 	float fallingSpeed = 0;
 	int gameStateFlag = 0;
+	Texture topTube;
+	Texture bottomTube;
+	int spaceBetweenTubes = 500;
 
 	@Override
 	public void create () {
@@ -23,16 +26,25 @@ public class FlappyBird extends ApplicationAdapter {
 		bird[0] = new Texture("bird_wings_up.png");
 		bird[1] = new Texture("bird_wings_down.png");
 		flyHeight = Gdx.graphics.getHeight() / 2 - bird[0].getHeight() / 2;
+		 topTube = new Texture("top_tube.png");
+		bottomTube = new Texture("bottom_tube.png");
 	}
 
 	@Override
 	public void render () {
+		batch.begin();
+		batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+
 		if(Gdx.input.justTouched()){
 			gameStateFlag = 1;
 		}
 		if(gameStateFlag ==1){
+
+
+		    batch.draw(topTube, Gdx.graphics.getWidth() / 2 - topTube.getWidth() / 2,Gdx.graphics.getHeight() / 2 + spaceBetweenTubes /2 );
+		    batch.draw(bottomTube, Gdx.graphics.getWidth() / 2 - bottomTube.getWidth() / 2,Gdx.graphics.getHeight() / 2 - spaceBetweenTubes /2 - bottomTube.getHeight() );
 			if(Gdx.input.justTouched()){
-				fallingSpeed = -30;
+				fallingSpeed = -20;
 			}
 if(flyHeight > 0 || fallingSpeed < 0){
 	  fallingSpeed++;
@@ -46,8 +58,7 @@ if(flyHeight > 0 || fallingSpeed < 0){
 	    if(birdStateFlag == 0){
 	        birdStateFlag = 1;
         }else birdStateFlag =0;
-batch.begin();
-batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+
 batch.draw(bird[birdStateFlag], Gdx.graphics.getWidth() / 2 - bird[birdStateFlag].getWidth() / 2, flyHeight);
 batch.end();
 	}
